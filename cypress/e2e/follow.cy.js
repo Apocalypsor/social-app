61336f4da5f8b322dba65ef3a69e72a26a8ecafd
AkoZhu <zxuanbiao@outlook.com>
2022-12-17 19:19:29 -0500

    Fix follow.cy.js

--- cypress/e2e/follow.cy.js
+++ cypress/e2e/follow.cy.js
@@ -1,5 +1,5 @@
-describe('Test that we could post a new comment. ', () => {
-    it('passes if we post a new comment', async () => {
+describe('Test that we could follow another user. ', () => {
+    it('passes if we could follow another user', async () => {
         // launch the app
          cy.visit('http://localhost:3000');
         // check the button with caption 'Don't have an account? Sign up' exists
@@ -14,11 +14,15 @@ describe('Test that we could post a new comment. ', () => {
 
 
         // check if we are in the main page.
-        // cy.get("link").get('[aria-label="userCard-username"]').should('exist');
+
 
         cy.wait(5000).then(()=>{
-            cy.get('button').eq(13).click();
+            // cy.get('button').eq(13).click();
+            cy.get('[aria-label="side-suggestion"] .follow-button').eq(0).click();
         });
+
+        // cy.get()
+
 
 
 
